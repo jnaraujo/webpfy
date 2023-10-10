7db77da55a5462989a7e210c29cbd4273a13fcdb
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2023-10-10 13:34:18 -0300

    ✨ feat: improved ui

--- src/components/layouts/Form/Done.tsx
+++ src/components/layouts/Form/Done.tsx
@@ -1,7 +1,9 @@
 import FileComp from "@/components/File";
 import { Button } from "@/components/ui/button";
+import { ScrollArea } from "@/components/ui/scroll-area";
 import { ArrowDownToLine, Plus } from "lucide-react";
 import Link from "next/link";
+import { useMemo } from "react";
 
 interface ConvertedFile {
   name: string;
@@ -26,22 +28,29 @@ export default function Done({ downloadUrl, reset, convertedFiles }: Props) {
     (file) => new File([file.blob], `${file.name}.webp`),
   );
 
+  const fileView = useMemo(() => {
+    return files.map((file) => (
+      <FileComp
+        key={file.name}
+        onClick={downloadFile}
+        file={new File([file], `${file.name}.webp`)}
+      >
+        <ArrowDownToLine className="text-zinc-200" />
+      </FileComp>
+    ));
+  }, [files]);
+
   return (
     <div className="flex flex-col items-center">
       <h2 className="mt-8 text-center text-lg font-semibold text-zinc-200 md:text-2xl">
         Your images are ready to download!
       </h2>
-      <div className="mt-8 grid max-h-60 grid-cols-3 gap-4 overflow-y-auto pr-2 md:grid-cols-5">
-        {files.map((file) => (
-          <FileComp
-            key={file.name}
-            onClick={downloadFile}
-            file={new File([file], `${file.name}.webp`)}
-          >
-            <ArrowDownToLine className="text-zinc-200" />
-          </FileComp>
-        ))}
-      </div>
+
+      <ScrollArea className="mt-8 max-h-60">
+        <div className="mr-2 grid grid-cols-3 gap-4 md:grid-cols-5">
+          {fileView}
+        </div>
+      </ScrollArea>
 
       <div className="mt-20 flex flex-col gap-4 md:flex-row">
         <Button asChild>
