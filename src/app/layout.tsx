741dfd167ae88395db96750305a1d032dec28f1e
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2023-10-10 14:26:49 -0300

    ✨ feat: add cloudflare insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import clsx from "clsx";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
@@ -29,6 +30,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="dark">
+      <head>
+        <Script
+          defer
+          src="https://static.cloudflareinsights.com/beacon.min.js"
+          data-cf-beacon={`{"token": "${process.env.NEXT_PUBLIC_CLOUDFLARE_TOKEN}"}`}
+          strategy="afterInteractive"
+        />
+      </head>
       <body
         className={clsx(
           "min-h-screen bg-zinc-950 font-sans antialiased",
